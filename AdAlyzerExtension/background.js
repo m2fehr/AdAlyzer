1dc8487c89ec1e53d281d75600725d6e09f7c65c
Matthias Fehr <matthias.fehr1@gmail.com>
2016-04-28 15:49:02 +0200

    Reset Counter on refresh
    Resets Counter when a new page is loaded or refreshed. Also added a
    badge which indicates the Ad count

--- AdAlyzerExtension/background.js
+++ AdAlyzerExtension/background.js
@@ -45,26 +45,38 @@ chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
    	console.log("tabID: " + removedTabId + "replaced, length now: " + reqs.size);
 });
 
+//Reset the Counter when new url is loaded
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if (changeInfo.status === "loading") {
+		console.log("reseted counter for tab " + tabId);
+		reqs.set(tabId, 0);
+		ads.set(tabId, 0);
+	}
+});
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
     	var tabId = details.tabId;
       	if(tabId && tabId !== -1) {
 			var reqCount = reqs.get(tabId);
-			if(reqCount) {
+			if(typeof reqCount !== "undefined") {
 				reqs.set(tabId, reqCount + 1);
 			}
 			else {
 				reqs.set(tabId, 1);
 				console.log("new Tab was added to Map, Id: " + tabId);
 			}
-			if(details.url.indexOf('ad') !== -1) {
+			if(details.url.indexOf('ad') !== -1) {	//ToDo: Check for Ads
 				var adsCount = ads.get(tabId);
 				if(adsCount) {
-					ads.set(tabId, adsCount + 1);
+					adsCount = adsCount + 1;
+					ads.set(tabId, adsCount);
 				}
 				else {
+					adsCount = 1;
 					ads.set(tabId, 1);
 				}
+				chrome.browserAction.setBadgeText({text: adsCount.toString(), tabId: tabId});
 			} 
 		}
     },
