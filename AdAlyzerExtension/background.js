236b83458089e3f6c6f82f00d446bf9cac573fa5
Matthias Fehr <matthias.fehr1@gmail.com>
2016-04-28 10:48:09 +0200

    Separate value for each tab
    Now differentiate between tabs and display value accordingly

--- AdAlyzerExtension/background.js
+++ AdAlyzerExtension/background.js
@@ -1,7 +1,26 @@
-var imgCount = 0;
+var m = new Map();
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) { 
-		console.log("bg message received");
-		imgCount = message.imgCount;
+		console.log("bg message received, tabID: " + sender.tab.id + " img: " + message.imgCount);
+		m.set(sender.tab.id, message.imgCount);
 		
 	 });
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+   console.log("tabID: " + tabId + "closed");
+   m.delete(tabId);
+});
+
+//Map Overview
+/*
+var m = new Map();
+var key1 = 'key1';
+var key2 = {};
+var key3 = {};
+
+m.set(key1, 'value1');
+m.set(key2, 'value2');
+
+console.assert(m.has(key2), "m should contain key2.");
+console.assert(!m.has(key3), "m should not contain key3.");
+*/
--- AdAlyzerExtension/popup.js
+++ AdAlyzerExtension/popup.js
@@ -1,32 +1,19 @@
-/*function test() {
-	
-	var backgroundWindow = chrome.extension.getBackgroundPage();
-	var imgCount = backgroundWindow.imgCount;
-	var textSpan = document.getElementById('GDWC_wordsTotal');
-	textSpan.innerHTML = imgCount;
-	
-	
-}();*/
-
-
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-    var imgCount;
     // Cache a reference to the status display SPAN
     var textSpan = document.getElementById('GDWC_wordsTotal');
-    // Handle the bookmark form submit event with our addBookmark function
     var backgroundWindow = chrome.extension.getBackgroundPage();
-    if (backgroundWindow)
-	   imgCount = backgroundWindow.imgCount;
-    else
-        imgCount = "no bgWindow";
-    // Get the event page
-    //chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
-        // content.js into the current tab's HTML
-      //  eventPage.getPageDetails(onPageDetailsReceived);
-    //});
-	document.getElementById('GDWC_wordsTotal').innerHTML = imgCount;
+    var query = { active: true, currentWindow: true };
+
+    chrome.tabs.query(query, function(tabs) {
+        var currentTab = tabs[0];
+        console.log("currentTabID: " + currentTab.id);
+
+        if (backgroundWindow.m.has(currentTab.id))
+            textSpan.innerHTML = backgroundWindow.m.get(currentTab.id);
+        else
+            textSpan.innerHTML = "reload the page";
+        });
+    
 });
 
