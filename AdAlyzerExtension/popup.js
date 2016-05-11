cd6c3b825e54638a8a62ee21bba1af8591e8f1fd
Matthias Fehr <matthias.fehr1@gmail.com>
2016-05-11 19:56:59 +0200

    Page Rating implemented
    Temp Rating algorithm, colors and text matches accordingly

--- AdAlyzerExtension/popup.js
+++ AdAlyzerExtension/popup.js
@@ -2,17 +2,57 @@ $(function() {
     $( "#tabs" ).tabs();
 });
 
+//Returns color from green to red according to value (0 - 1)
+function getColor(value){
+    //value from 0 to 1
+    if (value < 0.0) {
+        value = 0.0;
+    } 
+    else {
+        if (value > 1.0)
+            value = 1.0;
+    }
+    var hue=((1-value)*120).toString(10);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
+
+//Returns string sehr tief/tief/mittel/hoch/sehr hoch according to value (0 - 1)
+function getClassificationText(value){
+    //value from 0 to 1
+    if (value < 0.2) {
+        return "sehr tief";
+    } 
+    else {
+        if (value < 0.4)
+            return "tief";
+        else {
+            if (value < 0.6)
+                return "mittel";
+            else {
+                if (value < 0.8)
+                    return "hoch";
+                else 
+                    return "sehr hoch";
+            }
+        }
+    }
+}
+
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-    // Cache a reference to the status display SPAN
+    // First Tab
     var reqsTotal = document.getElementById('reqsTotal');
     var adsTotal = document.getElementById('adsTotal');
     var trackerTotal = document.getElementById('trackerTotal');
     var contentTotal = document.getElementById('contentTotal');
     var DOMTime = document.getElementById('DOMTime');
     var LoadTime = document.getElementById('LoadTime');
-
-
+    // Second Tab
+    var adsRating = document.getElementById('adsRating');
+    var trackerRating = document.getElementById('trackerRating');
+    var PLTRating = document.getElementById('PLTRating');
+    var totalRating = document.getElementById('totalRating');
+    // Third Tab
     var tableBody = document.getElementById("reqTableBody");
 
 
@@ -24,15 +64,34 @@ window.addEventListener('load', function(evt) {
         var currentTabEntry = backgroundWindow.tabs.get(currentTab.id);
         var reqMap = currentTabEntry.reqMap;
 
-        //if (backgroundWindow.tabs.has(currentTab.id)) { //nötig????
-            reqsTotal.innerHTML = reqMap.size;
-            adsTotal.innerHTML = currentTabEntry.elements.ads;
-            trackerTotal.innerHTML = currentTabEntry.elements.tracker;
-            contentTotal.innerHTML = currentTabEntry.elements.content;
-            DOMTime.innerHTML = currentTabEntry.plt.dom + "ms";
-            LoadTime.innerHTML = currentTabEntry.plt.load + "ms";
-        //}
+        // First Tab
+        reqsTotal.innerHTML = reqMap.size;
+        adsTotal.innerHTML = currentTabEntry.elements.ads;
+        trackerTotal.innerHTML = currentTabEntry.elements.tracker;
+        contentTotal.innerHTML = currentTabEntry.elements.content;
+        DOMTime.innerHTML = currentTabEntry.plt.dom + "ms";
+        LoadTime.innerHTML = currentTabEntry.plt.load + "ms";
+
+        // Second Tab
+        var value1 = currentTabEntry.elements.ads / reqMap.size * 4;
+        adsRating.textContent = getClassificationText(value1);
+        adsRating.style.backgroundColor = getColor(value1);
+
+        var value2 = currentTabEntry.elements.tracker / reqMap.size * 7;
+        trackerRating.textContent = getClassificationText(value2);
+        trackerRating.style.backgroundColor = getColor(value2);
+
+        var value3 = currentTabEntry.plt.load / 4;
+        //if (value3 > 1.0)
+        //    value3 = 1.0;
+        PLTRating.textContent = getClassificationText(value3);
+        PLTRating.style.backgroundColor = getColor(value3);
+
+        var value4 = (value1 + value2 + value3) / 3;
+        totalRating.textContent = getClassificationText(value4);
+        totalRating.style.backgroundColor = getColor(value4);
         
+        // Third Tab
         while (tableBody.firstChild) {
             tableBody.removeChild(tableBody.firstChild);
         }
@@ -53,13 +112,24 @@ window.addEventListener('load', function(evt) {
                 tableBody.appendChild(row);
             }
         }); 
-        
-        //sorttable.makeSortable(document.getElementById("reqTable"));
-
-    
-
-
-        });
+    });
     
 });
+
+
+/* Set Color from red to green
+function getColor(value){
+    //value from 0 to 1
+    var hue=((1-value)*120).toString(10);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
+var len=20;
+for(var i=0; i<=len; i++){
+    var value=i/len;
+    var d=document.createElement('div');
+    d.textContent="value="+value;
+    d.style.backgroundColor=getColor(value);
+    document.body.appendChild(d);
+}
+*/
 
