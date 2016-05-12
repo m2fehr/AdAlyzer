70059cb04d023b52eda617acc6b2de585a3b2a12
dpeisker <dpeisker@hsr.ch>
2016-05-12 16:38:50 +0200

    Einfügen Teststring Einige Überarbeitung Beginn impl. regex

--- AdAlyzerExtension/parser.js
+++ AdAlyzerExtension/parser.js
@@ -27,13 +27,28 @@ function parse() {
 				var allText = rawFile.responseText;
 				console.log(allText.length);
 				//alert(allText);
-				easyListAsArray = allText.split('\n');
+
+				/*
+				TODO: Kommentar vor easyListAsArray = allText.split('\n'); entfernen
+				 */
+				//easyListAsArray = allText.split('\n');
+
+				/*
+				TODO: Entfernen, Teststring um Parser zu Testen!
+				 */
+				var teststring = "&ad_box_, -ad-banner., /ad-iframe., /au2m8_preloader/*, /contentad|, ://affiliate.$third-party, -120-600., /adServe/*$popup, ###AD_CONTROL_13, ##.six-ads-wrapper, games.com#@##Adcode, " +
+					"dormy.se,marthastewart.com#@##ad-background, earlyamerica.com,infojobs.net#@#.topads, ||4dsply.com^$third-party, ||62.27.51.163^$third-party,domain=~adlive.de.ip, ||d2ue9k1rhsumed.cloudfront.net^, " +
+					"||ticketkai.com/banner/, ||tipico.*/affiliate/$third-party, @@||code.jquery.com^$script,third-party,domain=uplod.it, |http://$image,third-party,domain=tinypic.com, beemp3s.org,mnn.com,streamtuner.me###adv, " +
+					"isup.me###container > .domain + p + br + center:last-child, localmoxie.com##.ads_tilte + .main_mid_ads, statistiks.co.uk,statistiks.com##.adz, mobilelikez.com##div > span:last-child > [id][class], " +
+					"@@.com/banners, @@||ykhandler.com/adframe.js, @@.com/*.js|$domain=openload.co, @@/adcode.js$domain=cityam.com|techworld.com, ! Game Empire Enterprises";
+				easyListAsArray = teststring.split(', ');
+
 				//alert(easyListAsArray);
 				console.log(easyListAsArray);
 				var whitelist = 0;
-
+console.log("beginne mit for-loop");
 				for(var i = 1; i < easyListAsArray.length; i++){
-					console.log("Start der for-loop");
+					//console.log("Start der for-loop");
 					//das erste Element der EasyList kann ignoriert werden, daher i = 1.
 
 					//temp ist das aktuell behandelte Element aus der EasyList.
@@ -54,7 +69,10 @@ function parse() {
 					var rule = {ExceptionRule: 0, HidingRule: 0, Options: 0, URLStart: 0, URLEnd: 0, URLWithSubdomain: 0, HidingOption: "", Matchrule: "", OptionList: [], DomainList: []};
 
 					console.log(temp);
-					console.log(rule);
+					//console.log(rule);
+
+					//gemäss Implementierung von AdBlocker Plus können Regeln fälschlicherweise mit " " oder \r beginnen bzw. enden. Muss behoben werden damit keine Fehler bei der weiteren Verarbeitung entstehen.
+					temp = temp.replace(/\r$/, '').trim();
 
 					//Ignorieren falls Kommentar (regel beginnt mit !)
 					if(temp.charAt(0) == '!'){
@@ -80,21 +98,15 @@ function parse() {
 						if(whitelist == 0) {
 							//Ist nicht Whitelist. Ist Ausnahmeregel und keine Werbung.
 							rule.ExceptionRule = 1;
-							temp.slice(2);
+							temp = temp.slice(2);
 						}else{
 							//Ist Whitelist. Ist Werbung, welche jedoch nicht blockiert wird.
 							//Entfernen der @@ und weiterfahren wie mit normaler Regel.
-							temp.slice(2);
+							temp = temp.slice(2);
 						}
 					}
 
-					/*TODO:
-					Behandeln der zu versteckenden Elemente (###, ##., ##)
-
-					--> Beachten ob Whitelist (ebenfalls werbung)
-					--> Ausnahmeregeln (#@#, #@##) hier regeln oder bei matchrule?
-					 */
-
+					//Test auf Hiding-Rule (###, ##, #@##, #@#)
 					var hpos = temp.indexOf("#");
 
 					//Testen, ob # in der Regel vorkommt. Wenn nicht, kann es sich nicht um eine HideRule handeln und muss hier nicht weiter bearbeitet werden.
@@ -116,11 +128,12 @@ function parse() {
 								 Aktuell werden diese spezialregeln ignoriert und wie normale ids unbehandelt hinzugefügt.
 								 */
 
-								//id als Matchrule zu rule hinzufügen.
-								rule.Matchrule = temp;
-
-								//verarbeitung des Elementes abgeschlossen.
-								continue;
+								////id als Matchrule zu rule hinzufügen.
+								//rule.Matchrule = temp;
+                                //
+								////verarbeitung des Elementes abgeschlossen.
+								//rule_List.push(rule);
+								//continue;
 							}
 							//test ob ##. (class)
 							else if(temp.indexOf("##.") != -1){
@@ -130,11 +143,12 @@ function parse() {
 								//Löschen von ##.
 								temp.slice(3);
 
-								//class als matchrule zu rule hinzufügen.
-								rule.Matchrule = temp;
-
-								//verarbeitung des Elementes abgeschlossen.
-								continue;
+								////class als matchrule zu rule hinzufügen.
+								//rule.Matchrule = temp;
+                                //
+								////verarbeitung des Elementes abgeschlossen.
+								//rule_List.push(rule);
+								//continue;
 							}
 
 							//testen ob ##
@@ -146,16 +160,19 @@ function parse() {
 								temp.slice(2);
 
 								//Element als matchrule zu rule hinzufügen.
-								rule.Matchrule = temp;
 
-								/*
-								TODO:
-								temp enthält (nach entfernen der ##) noch den html-tag sowie Klammern und allenfalls Sonderzeichen (Bsp: div[id^="div-adtech-ad-"]) muss bereinigt bzw. zu regex umgeformt werden.
-								allenfalls kann der html-tag auch als HidingOption hinzugefügt werden.
-								 */
-
-								//verarbeitung des Elementes abgeschlossen.
-								continue;
+								//rule.Matchrule = temp;
+                                //
+								///*
+								//TODO:
+								//temp enthält (nach entfernen der ##) noch den html-tag sowie Klammern und allenfalls Sonderzeichen (Bsp: div[id^="div-adtech-ad-"]) muss bereinigt bzw. zu regex umgeformt werden.
+								//allenfalls kann der html-tag auch als HidingOption hinzugefügt werden.
+								// */
+                                //
+								////verarbeitung des Elementes abgeschlossen.
+								//rule_List.push(rule);
+                                //
+								//continue;
 							}
 
 							else{
@@ -184,10 +201,13 @@ function parse() {
 								rule.DomainList.push(substrng[ii]);
 							}
 
-							//id als matchrule speichern.
-							rule.Matchrule = temp;
-
-							continue;
+							////id als matchrule speichern.
+							//rule.Matchrule = temp;
+                            //
+							////verarbeitung des Elementes abgeschlossen.
+							//rule_List.push(rule);
+                            //
+							//continue;
 						}
 
 						else if(temp.indexOf("##.") != -1 && temp.indexOf("@#") == -1){
@@ -210,10 +230,13 @@ function parse() {
 								rule.DomainList.push(substrngx[iix]);
 							}
 
-							//class als matchrule speichern
-							rule.Matchrule = temp;
-
-							continue;
+							////class als matchrule speichern
+							//rule.Matchrule = temp;
+                            //
+							////verarbeitung des Elementes abgeschlossen.
+							//rule_List.push(rule);
+                            //
+							//continue;
 						}
 
 						else if(temp.indexOf("##") != -1 && temp.indexOf("@#") == -1){
@@ -235,16 +258,19 @@ function parse() {
 								rule.DomainList.push(substrngy[iiq]);
 							}
 
-							//html-tag info als matchrule speichern
-							rule.Matchrule = temp;
-
-							/*
-							 TODO:
-							 temp enthält (nach entfernen der ##) noch den html-tag (falls einer vorhanden) sowie Klammern und allenfalls Sonderzeichen (Bsp: div[id^="div-adtech-ad-"]) muss bereinigt bzw. zu regex umgeformt werden.
-							 allenfalls kann der html-tag auch als HidingOption hinzugefügt werden.
-							 */
-
-							continue;
+							////html-tag info als matchrule speichern
+							//rule.Matchrule = temp;
+                            //
+							///*
+							// TODO:
+							// temp enthält (nach entfernen der ##) noch den html-tag (falls einer vorhanden) sowie Klammern und allenfalls Sonderzeichen (Bsp: div[id^="div-adtech-ad-"]) muss bereinigt bzw. zu regex umgeformt werden.
+							// allenfalls kann der html-tag auch als HidingOption hinzugefügt werden.
+							// */
+                            //
+							////verarbeitung des Elementes abgeschlossen.
+							//rule_List.push(rule);
+                            //
+							//continue;
 						}
 
 						//Behandeln der Ausnahmeregeln #@## (ID), #@#.(class)
@@ -267,10 +293,13 @@ function parse() {
 								rule.DomainList.push(substrngexep[countx]);
 							}
 
-							//id als matchrule speichern
-							rule.Matchrule = temp;
-
-							continue;
+							////id als matchrule speichern
+							//rule.Matchrule = temp;
+                            //
+							////verarbeitung des Elementes abgeschlossen.
+							//rule_List.push(rule);
+                            //
+							//continue;
 						}
 
 						else if(temp.indexOf("#@#.") != -1){
@@ -291,10 +320,13 @@ function parse() {
 								rule.DomainList.push(substrngexep2[county]);
 							}
 
-							//id als matchrule speichern
-							rule.Matchrule = temp;
-
-							continue;
+							////id als matchrule speichern
+							//rule.Matchrule = temp;
+                            //
+							////verarbeitung des Elementes abgeschlossen.
+							//rule_List.push(rule);
+                            //
+							//continue;
 						}
 
 						else{
@@ -348,6 +380,12 @@ function parse() {
 					 URLEnd (Zeichen | am ende der regel) ACHTUNG: Nicht immer nur ein | am ende. Testen wie korrekt. | wird auch zum Aufzählen von Domains verwendet.
 					 URLWithSubdomain (Zeichen || am anfang der regel)
 					 */
+					if(/\|/.test(temp)){
+						if(/^\|\|/.test(temp)){
+							temp=temp.slice(2);
+
+						}
+					}
 
 					/*
 					 TODO:
@@ -358,10 +396,11 @@ function parse() {
 					TODO: Important!!!
 					bei den continue's werden die rules zwar korrekt erstellt, jedoch nicht zur rulelist hinzugefügt da die for-loop vorher verlassen wird. Wie regeln? verschieben des rule_List.push, aufruf vor continue oder ohne continue?
 					 */
+					console.log('\n' + temp + '\n' + rule.DomainList+ "     " + rule.OptionList + '\n' + '\n');
 					rule_List.push(rule);
 				}
 
-				console.log(matching_rules);
+				//console.log(matching_rules);
 				console.log("parsing finished");
 
 			}
@@ -369,11 +408,11 @@ function parse() {
 	};
 	rawFile.send();
 
-};
+}
 
 function match(url) {
 	console.log("matching url: " + url);
-};
+}
 
 
 /*
